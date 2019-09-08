e5db59007b9549e2c667686745a2a0fced4a9aba
Josh Bell <blitz43p@gmail.com>
2019-09-08 10:15:09 -0500

    Fixed the date object to be exactly at kickoff

--- js/countdown.js
+++ js/countdown.js
@@ -51,6 +51,6 @@
   }// end of initializeClock function
 
   // 7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order):
-  var deadline = new Date(2019,8,10);
+  var deadline = new Date(2019, 08, 08, 19, 20, 00, 0);
 
   initializeClock('clockdiv', deadline);
